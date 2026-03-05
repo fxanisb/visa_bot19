359bd1487a5a0c4dc08cca5308d422cc38f66841
ANIS BARKAT <anisbarkat93@gmail.com>
2026-03-05 16:05:24 +0100

    python check.py

--- check.py
+++ check.py
@@ -4,7 +4,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import time
 
@@ -102,6 +101,7 @@ while True:
         print("Error:", e)
 
         time.sleep(50)
+
 
 
 
